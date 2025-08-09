baa3fcc64efceb54c271ce866aa88d02fe851300
Samuel Flavio Brizzi <samuel.brizzi94@gmail.com>
2025-08-09 12:12:22 +0000

    removed junk deps

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 import './globals.css';
-import { Toaster } from "@/components/ui/toaster";
 
 export default function RootLayout({
   children,
@@ -16,7 +15,6 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         {children}
-        <Toaster />
       </body>
     </html>
   );
